13b2f7ee7cde6a808b4eede1b96bb4c1b1d59786
Sanket Deshmukh <deshmukhsanket19@gmail.com>
2022-02-26 15:34:04 +0530

    Navbar added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./componants/Navbar";
+import Shop from "./componants/Shop";
+
+function App() {
+  return <>
+  <Navbar/>
+  <Shop/>
+  </>;
+}
+
+export default App;
---
+++ src/componants/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div class="container">
+        <h2>Buy Reebok shoe at 50 rs</h2>
+        <button className="btn-primary mx-1" >-</button>
+    Add to cart
+          <button className="btn-primary mx-1">+</button>
+          
+          
+    </div>
+  )
+}
+
+export default Shop
